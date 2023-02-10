b1710eb11ce379a5db9c5ae84dd52aebc767be11
Ale-Coeto <alecoeto@hotmail.com>
2023-02-09 22:44:32 -0600

    V2

---
+++ Vision.py
@@ -0,0 +1,66 @@
+# Vision - By: alecoeto - mié feb 1 2023
+
+import sensor, image, time, math, pyb
+from pyb import UART
+
+#Thresholds (LAB)
+amarillo = ((52, 97, -25, 10, 33, 90))
+azul = ((21, 60, -6, 30, -74, -28))
+
+blue_led = pyb.LED(3)
+red_led = pyb.LED(1)
+
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time = 2000)
+sensor.set_auto_gain(False)
+sensor.set_auto_whitebal(False)
+
+clock = time.clock()
+uart = UART(3, 9600, timeout_char=1000)   #Comunicación serial por medio de UART
+
+while(True):
+    clock.tick()
+    img = sensor.snapshot()
+    #red_led.off()
+
+    count = 0
+
+    x = 'a'
+    for blob in img.find_blobs([amarillo], pixels_threshold=200, area_threshold=300, merge=True):
+        img.draw_rectangle(blob.rect(), color=(255,255,0))
+        img.draw_cross(blob.cx(), blob.cy(), color=(255,255,0))
+        img.draw_keypoints([(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20)
+        red_led.on()
+
+        if count == 0:
+            area = blob.area()
+            x = str(blob.cx()*10//320)
+        count += 1
+
+
+        if blob.area() > area:
+            x = str(blob.cx()*10//320)
+
+        area = max(blob.area(), area)
+    uart.write(x)
+
+    if (uart.any()):
+        uart.write(x)
+        colorIn = uart.read()
+        print(colorIn)
+
+        if colorIn == bytes("y", "ascii"):
+            uart.write("si")
+
+    #for blob in img.find_blobs([azul], pixels_threshold=200, area_threshold=300, merge=True):
+        #img.draw_rectangle(blob.rect(), color=(0,196,255))
+        #img.draw_cross(blob.cx(), blob.cy(), color=(0,196,255))
+        #img.draw_keypoints([(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20)
+        #red_led.on()
+        #x = blob.cx()*10//320
+        #uart.write(str(x))
+
+
